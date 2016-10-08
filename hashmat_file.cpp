b4d3cae4ebb4b79db67adfd250a35ec26e5e84a3
jayAlvarez <underated28@yahoo.com>
2016-10-08 09:16:04 +0800

    Add files via upload

---
+++ hashmat_file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	ifstream f("sample.txt");
+	int hashmat, opponent;
+	
+	while (f >> hashmat >> opponent) {
+		cout << abs(hashmat - opponent) << endl;
+	}
+	system("pause");
+	return 0;
+}
